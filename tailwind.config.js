26972b02f77cdfbbb1b5f2d08f230398f6888249
orindholt <u75087@rts-365.dk>
2022-06-24 02:40:32 +0200

    Remove unused animations and add red color

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
 				blue: "#2948ff",
 				green: "#81b29a",
 				orange: "#fb5607",
+				red: "#e63946",
 				gray: "#e5e5e5",
 				darkGray: "#8a8a8a",
 				black: "#2f2f2f",
@@ -19,14 +20,6 @@ module.exports = {
 					"0%": { opacity: 0, transform: "translateY(-6px)" },
 					"100%": { opacity: 1, transform: "translateY(0)" },
 				},
-				rainbow: {
-					"0%": { color: "#714eb3" },
-					"16.6%": { color: "#5286da" },
-					"33.3%": { color: "#6dbb5b" },
-					"49.9%": { color: "#eddf2b" },
-					"66.6%": { color: "#f98d52" },
-					"83.3%": { color: "#e14e65" },
-				},
 				wiggle: {
 					"0%, 100%": { transform: "rotate(-4deg)" },
 					"50%": { transform: "rotate(4deg)" },
@@ -35,7 +28,6 @@ module.exports = {
 			animation: {
 				fadeIn: "fadeIn 0.5s ease-out forwards",
 				delayFadeIn: "fadeIn 0.5s 0.25s ease-out forwards",
-				rainbow: "rainbow 10s ease-in-out infinite",
 				wiggle: "wiggle 1s ease-in-out infinite",
 			},
 		},
